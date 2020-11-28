1995415b5e405665fa7d3a4f76bd3cc305e82909
kdeng00 <kundeng94@gmail.com>
2020-11-28 08:32:39 -0500

    Starting over

---
+++ SpotifyDemo/SpotifyDemo.cpp
@@ -0,0 +1,12 @@
+﻿// SpotifyDemo.cpp : Defines the entry point for the application.
+//
+
+#include "SpotifyDemo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
